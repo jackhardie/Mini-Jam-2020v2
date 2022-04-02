e8fee6083acee217df96636096dc1a09614e0853
Vittorio CHIUSELLI <vittorio.chiuselli@gmail.com>
2022-04-03 00:08:43 +0200

    Add animation when die

--- Assets/Scripts/DeathHandler.cs
+++ Assets/Scripts/DeathHandler.cs
@@ -8,14 +8,14 @@ public class DeathHandler : MonoBehaviour {
 
     SanityManager sanityManager;
     FirstPersonController player;
-    Quaternion targetRotation;
-    Transform targetPosition;
     public GameObject cameraPlayer;
     public GameObject shadowSelf;
     bool isDead;
+    bool shadowSpawned;
     [SerializeField] float speedRotation = 1f;
     [SerializeField] float shadowDistance = 3f;
     [SerializeField] float shadowSpawnTime = 2f;
+    [SerializeField] Animator animator;
     public bool IsDead() {
         return isDead;
     }
@@ -29,19 +29,17 @@ public class DeathHandler : MonoBehaviour {
     void Update() {
         if (sanityManager.GetCurrentSanity() < 0 && !isDead || Input.GetKeyDown(KeyCode.P)) {
             isDead = true;
-            cameraPlayer.transform.position += new Vector3(0.0f, 0.0f, 2f);
-            targetRotation = Quaternion.LookRotation(-transform.forward, Vector3.up);
+            animator.enabled = true;
             StartCoroutine("SpawnShadow");
         }
         if (isDead)
             DeathEvent();
-    }
 
+    }
     private void DeathEvent() {
         player.enabled = false;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = false;
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speedRotation * Time.deltaTime);
     }
 
     IEnumerator SpawnShadow() {
