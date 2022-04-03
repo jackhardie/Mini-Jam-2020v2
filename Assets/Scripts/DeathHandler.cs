2bdf4cf684cc3848608a1be17d4b3c5a767de73e
Vittorio CHIUSELLI <vittorio.chiuselli@gmail.com>
2022-04-03 18:58:41 +0200

    Add dead by frontal controller

--- Assets/Scripts/DeathHandler.cs
+++ Assets/Scripts/DeathHandler.cs
@@ -12,32 +12,45 @@ public class DeathHandler : MonoBehaviour {
     public GameObject shadowSelf;
     bool isDead;
     bool shadowSpawned;
+    bool isKilledByAttack;
     [SerializeField] float speedRotation = 1f;
     [SerializeField] float shadowDistance = 3f;
     [SerializeField] float shadowSpawnTime = 2f;
-    [SerializeField] Animator animator;
+    [SerializeField] Animator animatorDeathBySanity;
     Transform startDeathPosition;
     public bool IsDead() {
         return isDead;
+    }
+    public bool GetIsKilledByAttack() {
+        return isKilledByAttack;
     }
 
     void Start() {
         isDead = false;
         sanityManager = GetComponent<SanityManager>();
         player = FindObjectOfType<FirstPersonController>();
+
     }
 
     void Update() {
         if (sanityManager.GetCurrentSanity() < 0 && !isDead || Input.GetKeyDown(KeyCode.P)) {
+            GetComponent<Animator>().SetBool("deadBySanity", true);
             isDead = true;
             startDeathPosition = cameraPlayer.transform;
             DeathEvent();
             DeathAnimation();
         }
+
+        if (isKilledByAttack || Input.GetKeyDown(KeyCode.K)) {
+            animatorDeathBySanity.enabled = true;
+            GetComponent<Animator>().SetBool("deadByFrontal", true);
+            isDead = true;
+        }
+
     }
 
     private void DeathAnimation() {
-        animator.enabled = true;
+        animatorDeathBySanity.enabled = true;
     }
 
     private void DeathEvent() {
