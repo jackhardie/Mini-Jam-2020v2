a17d46acdcd3aa8c91d89e12a7cc496a27c44a9c
Vittorio CHIUSELLI <vittorio.chiuselli@gmail.com>
2022-04-21 12:02:28 +0200

    Stamina decrease with the correct button

--- Assets/Scripts/Stamina.cs
+++ Assets/Scripts/Stamina.cs
@@ -19,11 +19,11 @@ public class Stamina : MonoBehaviour {
     }
 
     void Update() {
-        if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch)!=0f && !resting) { //running
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch)!=0f && !resting) { //running
             currentStamina -= loseStaminaOverTime * Time.deltaTime;
             if (currentStamina < staminaRunThreshold) {
                 resting = true;
-                player.SetRunSpeed(5f);
+       
             }
         }
         else {
